cfbf2498fb4ee867464f23fe986ad30eabfd093e
Syhwh <shymmywgarcia@gmail.com>
2022-02-16 07:53:17 -0500

    Fix: add jsx extentions

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { MainPage } from './Pages/MainPage';
-import Prov from './ThemeProvider/ThemeProvider';
+import ThemeProvider from './ThemeProvider/ThemeProvider';
 import './style.css';
 
 export default function App() {
   return (
-    <Prov>
+    <ThemeProvider>
       <MainPage />
-    </Prov>
+    </ThemeProvider>
   );
 }
--- src/ThemeProvider/ThemeProvider.jsx
+++ src/ThemeProvider/ThemeProvider.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { ThemeProvider } from 'theme-ui';
 import { theme } from './theme';
-import components from './components';
 
 export default (props) => (
   <ThemeProvider theme={theme}>{props.children}</ThemeProvider>
